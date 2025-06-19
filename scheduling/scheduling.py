54bfb2eb97e92711e0683c94705215e80f5e13d6
Quang00 <quangphong018@gmail.com>
2025-06-19 08:54:02 +0200

    update pga lenght

--- scheduling/scheduling.py
+++ scheduling/scheduling.py
@@ -1,31 +1,78 @@
-import math
+from scipy.stats import binom
 
-def length_pga(k: int, p_packet: float, p_gen: float, p_swap: float, n_swap: float, time_slot: float) -> float:
-    """
-    Calculate the length of a PGA (Packet Generation Attempt).
 
-    Parameters:
-    k (int): The number of succcess.
-    p_gen (float): The probability of generating an EPR pair in a given time slot.
-    p_packet (float): The probability of a packet being successfully generated.
-    p_swap (float): The probability of a successful swap operation.
-    n_swap (int): The number of swaps attempted.
-    time_slot (float): The duration of a time slot in microseconds.
+def probability_e2e(
+    n_swap: int, p_gen: float = 0.001, p_swap: float = 0.5, memory_size: int = 0
+) -> float:
+    """Calculate the end-to-end probability of generating EPR pairs in a given path.
+
+    Args:
+        n_swap (int): Number of swaps performed.
+        p_gen (float, optional): Probability of generating an EPR pair in a single trial.
+        p_swap (float, optional): Probability of swapping an EPR pair in a single trial.
+        memory_size (int, optional): Memory size in number of time units.
 
     Returns:
-    float: The length of the PGA in microseconds.
+        float: End-to-end probability of generating EPR pairs.
     """
-    if n_swap < 0:
-        raise ValueError("Number of swaps (n_swap) must be non-negative.")
-    if n_swap == 0:
-        p_e2e = p_gen
-    else:
-        p_e2e = (p_gen)**(2 * n_swap) * p_swap * n_swap
-    
-    n = 100
-    p_succ = math.comb(n, k) * (p_e2e)**k * (1 - p_e2e)**(n - k)
-    print(f"Probability of success: {p_succ}")
-    
-    return p_succ
+    p_link = 1 - (1 - p_gen) ** (memory_size + 1)
+    p_all_links = p_link ** (n_swap + 1)
+    p_bsms = p_swap**n_swap
 
-length_pga(2, 0.2, 10e-3, 0.5, 0, 1e-6)
+    return p_all_links * p_bsms
+
+
+def exceeds_p_packet(n: int, k: int, p_e2e: float, p_packet: float) -> bool:
+    """Check if the probability of generating at least k EPR pairs in n trials is greater than or equal to p_packet.
+
+    Args:
+        n (int): Number of trials.
+        k (int): Number of successes (number of EPR pairs generated).
+        p_e2e (float): Probability of generating an EPR pair end-to-end in a single trial.
+        p_packet (float): Probability of a packet being generated.
+
+    Returns:
+        bool: True if the probability of generating at least k EPR pairs in n trials is greater than or equal to p_packet.
+    """
+    return binom.sf(k - 1, n, p_e2e) >= p_packet
+
+
+def length_pga(
+    p_packet: float,
+    time_slot: float,
+    k: int,
+    n_swap: int,
+    p_swap: float = 0.5,
+    p_gen: float = 0.001,
+    memory_size: int = 0,
+) -> float:
+    """Calculate the length of a PGA (Packet Generation Attempt).
+
+    Args:
+        p_packet (float): Probability of a packet being generated.
+        time_slot (float): Length of a time slot in microseconds.
+        k (int): Number of successes (number of EPR pairs generated).
+        n_swap (int): Number of swaps performed.
+        p_swap (float, optional): Probability of swapping an EPR pair in a single trial.
+        p_gen (float, optional): Probability of generating an EPR pair in a single trial.
+        memory_size (int, optional): Memory size in number of time units.
+
+    Returns:
+        float: Length of a PGA.
+    """
+    p_e2e = probability_e2e(n_swap, p_gen, p_swap, memory_size)
+
+    low = k
+    high = low
+    while not exceeds_p_packet(high, k, p_e2e, p_packet):
+        high *= 2
+
+    # binary search
+    while low < high:
+        mid = (low + high) // 2
+        if exceeds_p_packet(mid, k, p_e2e, p_packet):
+            high = mid
+        else:
+            low = mid + 1
+    print(f"Binary search completed: low={low}, high={high}, p_e2e={p_e2e}")
+    return low * time_slot
