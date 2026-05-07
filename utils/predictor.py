ed3476c43ecc7fec34fb233ffddc59e9a6e7d7c6
Quang00 <81580063+Quang00@users.noreply.github.com>
2026-05-07 10:33:54 +0200

    refactor

--- utils/predictor.py
+++ utils/predictor.py
@@ -14,13 +14,11 @@ def topology_stats(gml_path: str) -> dict[str, float]:
     G = nx.read_gml(gml_path)
     disjoint_path_counts = []
     hops_count_all_paths = []
-    hops_count_shortest_paths = []
 
     for u, v in combinations(G.nodes(), 2):
         paths = list(nx.edge_disjoint_paths(G, u, v))
         disjoint_path_counts.append(len(paths))
         hops_count_all_paths.extend(len(path) - 1 for path in paths)
-        hops_count_shortest_paths.append(nx.shortest_path_length(G, u, v))
 
     avg_disjoint_paths = (
         sum(disjoint_path_counts) / len(disjoint_path_counts)
@@ -32,11 +30,7 @@ def topology_stats(gml_path: str) -> dict[str, float]:
         if hops_count_all_paths
         else 0
     )
-    avg_hops_shortest = (
-        sum(hops_count_shortest_paths) / len(hops_count_shortest_paths)
-        if hops_count_shortest_paths
-        else 0
-    )
+    avg_hops_shortest = nx.average_shortest_path_length(G)
 
     stats = {
         "avg_disjoint_paths": avg_disjoint_paths,
