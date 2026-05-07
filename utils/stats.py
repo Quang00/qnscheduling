e6fff5f855faa59bad71f14d584b6c24056a4bc6
Quang00 <quangphong018@gmail.com>
2026-05-07 08:34:12 +0200

    add predictor

--- utils/stats.py
+++ utils/stats.py
@@ -44,3 +44,40 @@ def topology_stats(gml_path: str) -> dict[str, float]:
         "avg_hops_shortest": avg_hops_shortest,
     }
     return stats
+
+
+def predictor(gml_path: str) -> dict[str, float | str]:
+    """Predict whether a network topology is more suitable for static or
+    dynamic routing based on its statistics.
+
+    Args:
+        gml_path (str): GML file path representing the network topology.
+
+    Returns:
+        dict[str, float | str]: path_redundancy, path_stretch, score,
+        prediction.
+    """
+    stats = topology_stats(gml_path)
+    path_redundancy = stats["avg_disjoint_paths"] - 1
+    path_stretch = stats["avg_hops_all"] / stats["avg_hops_shortest"] - 1
+    if path_stretch == 0:
+        score = 0 if path_redundancy == 0 else float("inf")
+    else:
+        score = path_redundancy / path_stretch
+
+    if score == 0:
+        prediction = "equal"
+    elif path_redundancy < 0.3:
+        prediction = "static"
+    elif score >= 3.3:
+        prediction = "dynamic"
+    else:
+        prediction = "static"
+
+    topology_prediction = {
+        "path_redundancy": path_redundancy,
+        "path_stretch": path_stretch,
+        "score": score,
+        "prediction": prediction,
+    }
+    return topology_prediction
